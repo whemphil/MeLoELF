e640a90333ccec47efd26686cca36b398bb50dec
Wayne Hemphill <wayne.hemphill@colorado.edu>
2026-02-02 21:14:49 -0700

    Add files via upload

--- R/MeLoELF.R
+++ R/MeLoELF.R
@@ -362,6 +362,8 @@ BM.thresh <- function(data.actual.fwd,data.actual.rev,met='RSS',p=(1-T1.err),set
         false.neg=(cumsum(pos.dens)/sum(pos.dens))[which.min(abs(fit.dens$x-thresh))]
         text(x=1,y=0.85*max(fit.dens$y),pos=2,labels=paste0('T1 ≈ ',round(1-p,2),' (',round(100*(1-p)*(1-sum(pos.dens*mean(diff(fit.dens$x),na.rm=T)))),'%)'),cex=2,col='black')
         text(x=1,y=0.75*max(fit.dens$y),pos=2,labels=paste0('T2 ≈ ',round(false.neg,2),' (',round(100*false.neg*sum(pos.dens*mean(diff(fit.dens$x),na.rm=T))),'%)'),cex=2,col='black')
+        lines(fit.dens$x,pos.dens,lty='dotted',col='purple',lwd=2)
+        lines(fit.dens$x,(1-sum(pos.dens,na.rm = T))*dbeta(fit.dens$x,shape1 = fit.betasSIN$par[1],shape2 = fit.betasSIN$par[2]),lty='dotted',col='purple',lwd=2)
       }else{
         text(x=1,y=0.85*max(fit.dens$y),pos=2,labels=paste0('T1 ≈ ',round(1-p,2),' (≤',round(100*(1-p)),'%)'),cex=2,col='black')
         text(x=1,y=0.75*max(fit.dens$y),pos=2,labels=paste0('T2 ≈ ','n.d.',' (','≤5','%)'),cex=2,col='black')
@@ -373,6 +375,8 @@ BM.thresh <- function(data.actual.fwd,data.actual.rev,met='RSS',p=(1-T1.err),set
         thresh=fit.dens$x[min(which((cumsum(neg.dens)/sum(neg.dens))>p))]
         text(x=1,y=0.85*max(fit.dens$y),pos=2,labels=paste0('T1 ≈ ',round(1-p,2),' (',round(100*(1-p)*sum(neg.dens*mean(diff(fit.dens$x),na.rm=T))),'%)'),cex=2,col='black')
         text(x=1,y=0.75*max(fit.dens$y),pos=2,labels=paste0('T2 ≈ ',round(pbeta(thresh,shape1 = fit.betasSIN$par[1],shape2 = fit.betasSIN$par[2]),2),' (',round(100*pbeta(thresh,shape1 = fit.betasSIN$par[1],shape2 = fit.betasSIN$par[2])*(1-sum(neg.dens*mean(diff(fit.dens$x),na.rm=T)))),'%)'),cex=2,col='black')
+        lines(fit.dens$x,neg.dens,lty='dotted',col='purple',lwd=2)
+        lines(fit.dens$x,(1-sum(neg.dens,na.rm = T))*dbeta(fit.dens$x,shape1 = fit.betasSIN$par[1],shape2 = fit.betasSIN$par[2]),lty='dotted',col='purple',lwd=2)
       }else{
         thresh=qbeta(p2,fit.betasSIN$par[1],fit.betasSIN$par[2])
         text(x=1,y=0.85*max(fit.dens$y),pos=2,labels=paste0('T1 ≈ ','n.d.',' (','≤5','%)'),cex=2,col='black')
@@ -873,7 +877,7 @@ if(process){
 
   # quality control analyses
   if(!pre.ligated){
-    read.filt=nrow(data.actual.rev)/nrow(REV.Cm[rowSums(!is.na(REV.Cm))>0,])
+    read.filt=nrow(data.actual.rev)/sum(rowSums(!is.na(REV.Cm[,rev(REV.sites)]))>0,na.rm = T)
     fNA.fwd=colSums(is.na(fwd.binary))/nrow(fwd.binary)
     fNA.rev=colSums(is.na(rev.binary))/nrow(rev.binary)
     fCpGs.fwd=colSums(fwd.binary,na.rm = T)/colSums(!is.na(fwd.binary))
